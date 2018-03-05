4c6f41a5d7582b18b5b5436c0e23007d49776f13
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-03-05 18:26:24 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,8 +22,8 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
    FROM authors
-+  INNER JOIN series ON series.author_id=authors.id
-+  INNER JOIN subgenres ON subgenres.id=series.subgenre_id;"
+   INNER JOIN series ON series.author_id=authors.id
+   INNER JOIN subgenres ON subgenres.id=series.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
